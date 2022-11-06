0415d1b0571d8227ec9dcd57ca5634fa751c43c2
ErikT <etalma@me.com>
2022-11-06 14:24:41 +0100

    Change vane change update 1 = horizontal

--- components/mitsubishi_heatpump/espmhp.cpp
+++ components/mitsubishi_heatpump/espmhp.cpp
@@ -345,7 +345,7 @@ void MitsubishiHeatPump::hpSettingsChanged() {
         this->swing_mode = climate::CLIMATE_SWING_BOTH;
     } else if (strcmp(currentSettings.vane, "5") == 0) {
         this->swing_mode = climate::CLIMATE_SWING_VERTICAL;
-    } else if (strcmp(currentSettings.vane, "2") == 0) {
+    } else if (strcmp(currentSettings.vane, "1") == 0) {
         this->swing_mode = climate::CLIMATE_SWING_HORIZONTAL;
     } else {
         this->swing_mode = climate::CLIMATE_SWING_OFF;
